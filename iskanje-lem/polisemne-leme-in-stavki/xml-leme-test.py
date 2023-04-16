efd02bd9f6d2f2cc584da1949a03cff4f6f03351
Nives Hüll <nivea.miki@gmail.com>
2023-04-16 20:22:25 +0200

    Dodan postopek iskanja lem v že lematiziranem korpusu

---
+++ iskanje-lem/polisemne-leme-in-stavki/xml-leme-test.py
@@ -0,0 +1,59 @@
+import xml.etree.ElementTree as ET
+import csv
+
+polisemne = "polysemous_words.txt"
+with open(polisemne, 'r', encoding='utf-8') as f:
+    polysemous_lemmas = set([line.strip() for line in f.readlines()])
+
+stavki_s_polisemi = []
+
+with open('output.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Stavek', 'Polisemna lema'])
+
+    tree = ET.parse('prvi-xml-za-test.xml')
+    root = tree.getroot()
+    #print(root)
+
+    for odstavek_st, p in enumerate(root[1][0]):
+        polisem_found_in_par = False
+        #print("Nov odstavek", odstavek_st)
+
+        for poved_st, s in enumerate(p):
+            #print(f"\n- Odstavek št. {odstavek_st}, poved št. {poved_st}:")
+            stavek = ""
+            polisem_found = False
+            polisem_lemmas = []
+
+            for w in s:
+                #print(w.tag)
+                #print(f"Posamezna beseda: {w.text}")
+                if w.text == None:
+                    stavek += " "
+                else:
+                    stavek += str(w.text)
+
+                if w.tag == '{http://www.tei-c.org/ns/1.0}w':
+                    #print(w.attrib["lemma"], w.text)
+                    lemma = w.attrib["lemma"]
+                    #print("Lema: ", lemma)
+                    if lemma in polysemous_lemmas:
+                        polisem_found = True
+                        polisem_lemmas.append(lemma)
+                        #print(f">>> Lema '{lemma}' je polisem.")
+
+            if polisem_found:
+                stavki_s_polisemi.append((odstavek_st, poved_st, stavek))
+                polisem_found_in_par = True
+                #print(f"- Odstavek št. {odstavek_st}, poved št. {poved_st}:")
+                #print(f"Stavek: {stavek}")
+                writer.writerow([stavek, ';'.join(polisem_lemmas)])
+
+        if polisem_found_in_par == True:
+                print(f"- Polisem najden: odstavek št. {odstavek_st}.\n")
+                pass
+
+print(f"Št. stavkov, ki vsebujejo poliseme: {len(stavki_s_polisemi)}")
+print("\nStavki, ki vsebujejo poliseme:")
+for i, (odstavek_st, poved_st, stavek) in enumerate(stavki_s_polisemi):
+    print(f"{i+1}. (odstavek {odstavek_st}, poved {poved_st}): {stavek}")
---
+++ iskanje-lem/zmanjsevanje-inventarja-polisemnih-lem/zmanjsaj-inventar-polisemnih-besed.py
@@ -0,0 +1,10 @@
+with open("inventar-polisemnih-besed.tsv", "r") as f1:
+    senses = [line.strip() for line in f1]
+
+with open("polisemne_nives.txt", "r") as f2:
+    words = set([line.strip() for line in f2])
+
+filtered_senses = [sense for sense in senses if sense.split("\t")[0] in words]
+
+with open("inventar-polisemnih-zmanjsan.tsv", "w") as f3:
+    f3.write("\n".join(filtered_senses))
