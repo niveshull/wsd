1fa0bbd46e45113768136b7215bb504e3d74117a
Nives Hüll <nivea.miki@gmail.com>
2023-05-25 03:36:50 +0200

    poskus 2

--- iskanje-lem/polisemne-leme-in-stavki/ustvari_wic.py
+++ iskanje-lem/polisemne-leme-in-stavki/ustvari_wic.py
@@ -2,9 +2,7 @@ import random
 import csv
 
 def remove_quotes(sentence):
-    if sentence.startswith('"') and sentence.endswith('"'):
-        return sentence[1:-1]
-    return sentence
+    return sentence.strip('"')
 
 def create_wic_csv(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8-sig') as f:
@@ -20,11 +18,11 @@ def create_wic_csv(input_file, output_file):
             lemma = line.split()[1]
         elif ":" in line:
             cent, poved = line.split(":", 1)
-            sentence_pairs.append([lemma, remove_quotes(poved.strip()), ''])
+            sentence_pairs.append([lemma, poved.strip(), ''])  # Remove the call to remove_quotes function
 
     with open(output_file, 'w', encoding='utf-8-sig', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['lema', 'stavek_1', 'stavek_2', 'oznaka (label)'])  # Write header
+        writer.writerow(['lemma', 'stavek_1', 'stavek_2', 'oznaka (label)'])  # Write header
         writer.writerows(sentence_pairs)
 
     print(f"CSV file '{output_file}' has been created.")
