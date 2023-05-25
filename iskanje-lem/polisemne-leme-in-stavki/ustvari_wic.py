72c327d34264c20f9f887332695fc6b35ed37750
unknown <klaruh123@gmail.com>
2023-05-25 21:38:44 +0200

    wic ustvarjanje

--- iskanje-lem/polisemne-leme-in-stavki/ustvari_wic.py
+++ iskanje-lem/polisemne-leme-in-stavki/ustvari_wic.py
@@ -1,6 +1,7 @@
 import random
+import csv
 
-def create_wic_file(input_file, output_file):
+def create_wic_csv(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8-sig') as f:
         lines = f.readlines()
 
@@ -19,19 +20,24 @@ def create_wic_file(input_file, output_file):
                 groups.append(group)
             group = []
 
-    with open(output_file, 'w', encoding='utf-8-sig') as f:
-        for group in groups:
-            if len(group) > 1:
-                random.shuffle(group)
-                for i in range(len(group) - 1):
-                    sentence1 = group[i]
-                    sentence2 = group[i + 1]
-                    f.write(f"{sentence1}\t{sentence2}\n")
+    sentence_pairs = []
+    for group in groups:
+        if len(group) > 1:
+            random.shuffle(group)
+            for i in range(len(group) - 1):
+                sentence1 = group[i]
+                sentence2 = group[i + 1]
+                sentence_pairs.append([sentence1, sentence2])
 
-    print(f"WiC file '{output_file}' has been created.")
+    with open(output_file, 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['sentence1', 'sentence2'])  # Write header
+        writer.writerows(sentence_pairs)
+
+    print(f"CSV file '{output_file}' has been created.")
 
 # Usage example
 input_filename = 'a_test.txt'  # Replace with your input file name
-output_filename = 'b_test.wic'  # Replace with your output file name
+output_filename = 'b_test.csv'  # Replace with your output file name
 
-create_wic_file(input_filename, output_filename)
+create_wic_csv(input_filename, output_filename)
